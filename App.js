b533e3b4cca498d3c1ae093aba69a7b6e745d524
hiren0412 <hiren.vaghela@bacancytechnology.com>
2020-05-05 19:03:30 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import RegisterScreen from './src/screens/RegisterScreen';
+class App extends Component {
+  render() {
+    return (
+      <RegisterScreen />    
+    );
+  }
+}
+
+export default App
